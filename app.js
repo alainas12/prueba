2df9c8e8efc1e5743ad8689797d82790ac4d0d45
alainas12 <jalain_as@hotmail.com>
2023-11-28 12:23:08 -0600

    segunda test

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 function dividirNumeros(numero1, numero2){
-    return numero1/numero2;
-
+    try {
+        var resultadoDivision = numero1/numero2;
+    } catch (error) {
+        return "Error:" + error.message;
+    }
+    
+    
+    return resultadoDivision;
 }
-var resultadoDivision = dividirNumeros(10,2);
+var resultadoDivision = dividirNumeros(2,0);
 console.log(resultadoDivision);
